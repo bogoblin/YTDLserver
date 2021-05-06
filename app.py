b0ab815d9e3a0dd665f7b1eb863dd680f3a4c5fb
bogoblin <bogoblin@gmail.com>
2021-05-06 23:58:01 +0100

    Fix CORS error

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 import os
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
+from flask_cors import CORS
 import youtube_dl
 
 app = Flask(__name__)
-
-
-class MyLogger(object):
-    def debug(self, msg):
-        print('debug - '+msg)
-
-    def warning(self, msg):
-        print('warning - '+msg)
-
-    def error(self, msg):
-        print('error - '+msg)
+CORS(app)
 
 
 download_dir = r'D:\YouTube'
 
 
-def hook(d):
-    print(d)
-
-
 ydl_opts = {
-    'outtmpl': os.path.join(download_dir, "%(uploader)s", "%(title)s.%(ext)s"),
-    'progress_hooks': [hook]
+    'outtmpl': os.path.join(download_dir, "%(uploader)s", "%(title)s.%(ext)s")
 }
 
 
@@ -39,7 +25,7 @@ def home():
 def download():
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([request.form['url']])
-    return redirect('/')
+    return "Download complete", 200
 
 
 if __name__ == '__main__':
